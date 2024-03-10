f209c32029653af26186c898831b788a254bcd62
jxa <julianalbert0012@gmail.com>
2024-03-10 12:04:04 -0700

    Styling

--- src/components/ToolComponents/Port-Scan-component.js
+++ src/components/ToolComponents/Port-Scan-component.js
@@ -12,6 +12,7 @@ const portInfo = [
   { port: 110, protocol: 'POP3', description: 'Post Office Protocol version 3' },
   { port: 143, protocol: 'IMAP', description: 'Internet Message Access Protocol' },
   { port: 443, protocol: 'HTTPS', description: 'HTTP Secure' },
+  { port: 465, protocol: 'SMTP', description: 'SMTP over SSL' },
   { port: 3389, protocol: 'RDP', description: 'Remote Desktop Protocol' },
   // Add more ports and their details here...
 ];
@@ -19,8 +20,8 @@ const portInfo = [
 
 const PortScanner = () => {
   const [ipAddress, setIPAddress] = useState('');
-  const [startPort, setStartPort] = useState(1);
-  const [endPort, setEndPort] = useState(65535);
+  const [startPort, setStartPort] = useState();
+  const [endPort, setEndPort] = useState();
   const [scanResults, setScanResults] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -69,42 +70,44 @@ const PortScanner = () => {
   const openPortsDetails = processDiscoveredPorts();
 
   return (
-    <div className="port-scanner">
-      <div className="input-section">
-        <input
+      <div className="port-scanner">
+        <div className="input-section">
+          <input
             type="text"
             value={ipAddress}
             onChange={(e) => {
-                console.log(e.target.value); // Log the input value
-                setIPAddress(e.target.value);
+              console.log(e.target.value); // Log the input value
+              setIPAddress(e.target.value);
             }}
             placeholder="Enter IP address or URL"
-            className="input-field" // Apply CSS class for styling
-        />
-        <input
-            type="number"
-            value={startPort}
-            onChange={(e) => {
+            className="input-field input-ip-url" // Apply CSS class for styling
+          />
+          <div className="port-range-inputs"> {/* New wrapper for port inputs */}
+            <input
+              type="number"
+              value={startPort}
+              onChange={(e) => {
                 console.log(e.target.value); // Log the input value
                 setStartPort(e.target.value);
-            }}
-            placeholder="Start Port"
-            className="input-field" // Apply CSS class for styling
-        />
-        <input
-            type="number"
-            value={endPort}
-            onChange={(e) => {
+              }}
+              placeholder="Start Port"
+              className="input-field port-input" // Apply CSS class for styling
+            />
+            <input
+              type="number"
+              value={endPort}
+              onChange={(e) => {
                 console.log(e.target.value); // Log the input value
                 setEndPort(e.target.value);
-            }}
-            placeholder="End Port"
-            className="input-field" // Apply CSS class for styling
-        />
-        <button onClick={handleIPSubmit} disabled={loading} className="scan-button"> {/* Apply CSS class for styling */}
-          {loading ? 'Scanning...' : 'Scan'}
-        </button>
-      </div>
+              }}
+              placeholder="End Port"
+              className="input-field port-input" // Apply CSS class for styling
+            />
+          </div>
+          <button onClick={handleIPSubmit} disabled={loading} className="scan-button">
+            {loading ? 'Scanning...' : 'Scan'}
+          </button>
+        </div>
       {error && <p className="error-message">{error}</p>}
       {loading && <LoaderComp />} {/* Render the LoaderComp when loading is true */}
       {scanResults && (
@@ -127,18 +130,18 @@ const PortScanner = () => {
                 <thead>
                   <tr>
                     <th>Port #</th>
+                    <th>State</th>
                     <th>Application Layer Protocol</th>
                     <th>Description</th>
-                    <th>State</th>
                   </tr>
                 </thead>
                 <tbody>
                   {openPortsDetails.map((details) => (
                     <tr key={details.port}>
                       <td>{details.port}</td>
+                      <td><img src={`${process.env.PUBLIC_URL}/check.png`} alt="Open" /></td>
                       <td>{details.protocol}</td>
                       <td>{details.description}</td>
-                      <td><img src={`${process.env.PUBLIC_URL}/check.png`} alt="Open" /></td>
                     </tr>
                   ))}
                 </tbody>
